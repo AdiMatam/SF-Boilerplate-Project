000abdfcf17f0bb503493b1527774f9ce1e4ad5b
Aditya Matam <aditya0310@outlook.com>
2022-04-15 19:09:38 -0700

    Misc

--- Core/BaseScreen.cpp
+++
@@ -1,7 +0,0 @@
-#include "Pch.hpp"
-#include "BaseScreen.hpp"
-
-void BaseScreen::onUpdate() {}
-void BaseScreen::onEvent(const sf::Event& ev) {}
-void BaseScreen::setupGeometry() {}
-void BaseScreen::setupWidgets() {}
